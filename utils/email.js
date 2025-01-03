6e7f29caf69a0544e4687cc12ea0539ea06ab407
Hidan12 <marcarfe12@gmail.com>
2025-01-03 02:00:17 -0500

    fixed email

--- utils/email.js
+++ utils/email.js
@@ -55,11 +55,11 @@ export const welcomeEmail = ()=>{
             Martin Vidan
         </div>
         <div class="body">
-            <h1>Thanks for getting in touch!</h1>
+            <h1>Thanks for contacting me!!</h1>
             <p>Thank you very much for writing to me. I have received your message and I will get back to you as soon as possible.</p>
             <p>If you need something urgently, please feel free to reply to this email. In the meantime, I invite you to explore more about me and my projects in my portfolio.</p>
             <div class="button-container">
-                <a href="https://martin-vidan.netlify.app/" class="button">Ir a la página</a>
+                <a href="https://martin-vidan.netlify.app/" class="button">Back to portfolio</a>
             </div>
         </div>
     </div>
